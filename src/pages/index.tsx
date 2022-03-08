9db42c7c1718aabf05f38764b01fd3d4eec5e35e
Thomás Dixini <83423919+ThomasDixini@users.noreply.github.com>
2022-03-08 17:06:36 -0300

    Componente MenuAside

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { Header } from "../components/Header/Header"
+import { MenuAside } from "../components/MenuAside/MenuAside"
 
 
 export default function Home() {
   return (
     <>
       <Header />
+      <MenuAside />
     </>
   )
 }

642ed814693ceeda883a9169954ab146c1396675
Thomás Dixini <83423919+ThomasDixini@users.noreply.github.com>
2022-03-09 16:28:02 -0300

    Componente de início estruturado e estilizado

--- src/components/MenuAside/MenuAside.tsx
+++ src/components/MenuAside/MenuAside.tsx
@@ -5,7 +5,21 @@ import { Container } from "./styles";
 export function MenuAside() {
     return(
         <Container>
-
+            <img src="" alt="Spotfy Logo" />
+            <nav>
+                <a>
+                    <img src="" alt="" />
+                    Início
+                </a>
+                <a>
+                    <img src="" alt="" />
+                    Buscar
+                </a>
+                <a>
+                    <img src="" alt="" />
+                    Sua Biblioteca
+                </a>
+            </nav>
         </Container>
     );
 }
--- src/components/MenuAside/styles.ts
+++ src/components/MenuAside/styles.ts
@@ -3,6 +3,31 @@ import styled from 'styled-components';
 
 export const Container = styled.aside`
 
+    width: 23rem;
+    height: 100vh;
+    background-color: #000;
+
+    nav {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+
+        gap: 1rem;
+
+        margin-top: 4rem;
+
+        a {
+            padding: 1rem;
+            width: 70%;
+            margin: 0 auto;
+
+            display: flex;
+            text-align: left;
+
+            background-color: red ;
+        }
+    }
+
 
 
 

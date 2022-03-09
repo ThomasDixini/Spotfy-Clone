c7fa80037133c3b5ba2d03f19783746dd19cc757
Thomás Dixini <83423919+ThomasDixini@users.noreply.github.com>
2022-03-09 16:28:38 -0300

    Ajustando componentes em tela

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import { Header } from "../components/Header/Header"
 import { MenuAside } from "../components/MenuAside/MenuAside"
+import { Container } from "./home"
 
 
 export default function Home() {
   return (
-    <>
+    <Container>
       <Header />
       <MenuAside />
-    </>
+    </Container>
   )
 }
